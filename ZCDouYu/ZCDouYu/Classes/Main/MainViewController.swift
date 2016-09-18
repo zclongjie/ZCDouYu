8473b109d07224e3e2b54ed237c6e4758a02a810
赵隆杰 <zclongjie@gmail.com>
2016-09-18 15:36:09 +0800

    框架搭建

---
+++ ZCDouYu/ZCDouYu/Classes/Main/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  ZCDouYu
+//
+//  Created by 赵隆杰 on 16/9/18.
+//  Copyright © 2016年 赵隆杰. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
