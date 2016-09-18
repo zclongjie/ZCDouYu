141a8681190479ddc6c4897706dc12fe6209c2de
赵隆杰 <zclongjie@gmail.com>
2016-09-18 19:57:03 +0800

    首页布局TitleView,ContentView

---
+++ ZCDouYu/ZCDouYu/Classes/Main/View/PageContentView.swift
@@ -0,0 +1,9 @@
+//
+//  PageContentView.swift
+//  ZCDouYu
+//
+//  Created by 赵隆杰 on 16/9/18.
+//  Copyright © 2016年 赵隆杰. All rights reserved.
+//
+
+import Foundation
---
+++ ZCDouYu/ZCDouYu/Classes/Main/View/PageTitleView.swift
@@ -0,0 +1,13 @@
+//
+//  PageTitleView.swift
+//  ZCDouYu
+//
+//  Created by 赵隆杰 on 16/9/18.
+//  Copyright © 2016年 赵隆杰. All rights reserved.
+//
+
+import Cocoa
+
+class PageTitleView: UIView {
+
+}
